a142160edea3b32d477345e2702cb163bfaa7cc6
dillonmemo <arta1069@gmail.com>
2020-01-20 12:55:34 +0900

    test tinymce

--- client/src/Routes/tiny/index.tsx
+++ client/src/Routes/tiny/index.tsx
@@ -11,6 +11,7 @@ const TinyEditor: React.FC<TinyEditorProps> = ({}) => {
     <div>
       <h2>Default</h2>
       <Editor
+        apiKey="pvpw2z0ouubmjquu6398rg3vxyqch3rfrhuru6qsekuf49ne"
         initialValue="<p>This is the initial content of the editor</p>"
         init={{
           height: 300,
@@ -24,6 +25,22 @@ const TinyEditor: React.FC<TinyEditorProps> = ({}) => {
             "undo redo | formatselect | bold italic backcolor | \
              alignleft aligncenter alignright alignjustify | \
              bullist numlist outdent indent | removeformat | help"
+        }}
+        onChange={e => handleEditorChange(e)}
+      />
+      <h2>addons plugins + toolbar</h2>
+      <Editor
+        apiKey="pvpw2z0ouubmjquu6398rg3vxyqch3rfrhuru6qsekuf49ne"
+        initialValue="<p>This is the initial content of the editor</p>"
+        init={{
+          height: 300,
+          menubar: "insert edit",
+          plugins: ["a11ychecker", "lists", "advlist", "image", "imagetools", "paste"],
+          toolbar:
+            "undo redo | formatselect | bold italic backcolor | \
+             alignleft aligncenter alignright alignjustify | \
+             bullist numlist outdent indent | removeformat | help a11ycheck | \
+             image paste"
         }}
         onChange={e => handleEditorChange(e)}
       />
